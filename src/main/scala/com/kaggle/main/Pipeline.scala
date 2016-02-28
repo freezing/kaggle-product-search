4bbce637813555ad418abf1bc21234ea39d815dd
Nikola Stojiljkovic <nikolavla@gmail.com>
2016-02-28 03:05:03 +0000

    Pipeline (high level) finished

--- src/main/scala/com/kaggle/main/Pipeline.scala
+++ src/main/scala/com/kaggle/main/Pipeline.scala
@@ -1,6 +1,10 @@
 package com.kaggle.main
 
+import java.nio.file.Paths
+
+import com.kaggle.SubmitCsvCreator
 import com.kaggle.feature.extraction.SimpleFeatureExtractor
+import com.kaggle.ml.MachineLearning
 import com.kaggle.nlp.DataCleaner
 import com.kaggle.service.SparkFileReader
 
@@ -14,6 +18,10 @@ import com.kaggle.service.SparkFileReader
   * 5. Save results
   */
 object Pipeline extends App with Serializable {
+  val outputPath = args.toSeq sliding 2 collectFirst {
+    case Seq("--outputPath", path) => Paths.get(path)
+  } getOrElse { throw new Exception("No output path specified") }
+
   // 1. Read data
   val trainData = SparkFileReader.readTrainData("/train.csv")
   val testData = SparkFileReader.readTestData("/test.csv")
@@ -26,6 +34,10 @@ object Pipeline extends App with Serializable {
   val trainDataFeatures = SimpleFeatureExtractor.processTrainData(cleanTrainData)
   val testDataFeatures = SimpleFeatureExtractor.processTestData(cleanTestData)
 
-//  val path = Paths.get("/Users/freezing/Desktop/submit.csv")
-//  new SubmitCsvCreator(evaluations).save(path)
+  // 4. Machine Learning
+  val evaluations = MachineLearning.trainAndPredict(trainDataFeatures, testDataFeatures)
+
+  // 5. Save results
+  // TODO: Use Spark for this
+  new SubmitCsvCreator(evaluations.toLocalIterator.toList).save(outputPath)
 }
--- src/main/scala/com/kaggle/ml/MachineLearning.scala
+++ src/main/scala/com/kaggle/ml/MachineLearning.scala
@@ -1,8 +1,9 @@
 package com.kaggle.ml
 
-import com.kaggle.feature.Feature
+import com.kaggle.feature.{TestFeature, TrainFeature, Feature}
 import com.kaggle.model.{Evaluation, Id, Relevance}
 import org.apache.spark.ml.regression.LinearRegression
+import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.mllib.regression.{LinearRegressionModel, LabeledPoint, LinearRegressionWithSGD}
 import org.apache.spark.mllib.linalg._
@@ -10,33 +11,52 @@ import org.apache.spark.mllib.linalg._
 /**
   * Created by freezing on 2/26/16.
   */
-class MachineLearning(implicit val sc: SparkContext) extends Serializable {
-
-  var model: LinearRegressionModel = null
-
-  def train(trainingData: List[(Feature, Relevance)]): Unit = {
-    val training = sc.parallelize(createTrainingSet(trainingData)).cache()
-    model = LinearRegressionWithSGD.train(training, 2000, 0.2)
-  }
-
-  def evaluate(testData: List[(Id, Feature)]): List[Evaluation] = {
-    testData map { case (id, feature) =>
-      val prediction = Math.min(3.0, Math.max(model.predict(toVector(feature)), 1.0))
-      Evaluation(id, Relevance(prediction))
+class MachineLearning {
+  def train(data: RDD[TrainFeature]): LinearRegressionModel = {
+    val labeledData = data map { trainFeature =>
+      LabeledPoint(trainFeature.relevance.value, toVector(trainFeature.feature))
     }
+    LinearRegressionWithSGD.train(labeledData, 2000, 0.2, 1.0)
   }
 
-  private def createTestSet(data: List[(Id, Feature)]): Seq[Vector] = {
-    data map { case (id, feature) => toVector(feature) }
+  def predict(model: LinearRegressionModel, data: RDD[TestFeature]): RDD[Evaluation] = {
+    ???
   }
 
-  private def createTrainingSet(data: List[(Feature, Relevance)]): Seq[LabeledPoint] = {
-    data map { case (feature, Relevance(relevance)) =>
-      LabeledPoint(relevance, toVector(feature))
-    }
+  def trainAndPredict(trainData: RDD[TrainFeature], testData: RDD[TestFeature]): RDD[Evaluation] = {
+    val model = train(trainData)
+    predict(model, testData)
   }
 
+//class MachineLearning(implicit val sc: SparkContext) extends Serializable {
+//
+//  var model: LinearRegressionModel = null
+//
+//  def train(trainingData: List[(Feature, Relevance)]): Unit = {
+//    val training = sc.parallelize(createTrainingSet(trainingData)).cache()
+//    model = LinearRegressionWithSGD.train(training, 2000, 0.2)
+//  }
+//
+//  def evaluate(testData: List[(Id, Feature)]): List[Evaluation] = {
+//    testData map { case (id, feature) =>
+//      val prediction = Math.min(3.0, Math.max(model.predict(toVector(feature)), 1.0))
+//      Evaluation(id, Relevance(prediction))
+//    }
+//  }
+//
+//  private def createTestSet(data: List[(Id, Feature)]): Seq[Vector] = {
+//    data map { case (id, feature) => toVector(feature) }
+//  }
+//
+//  private def createTrainingSet(data: List[(Feature, Relevance)]): Seq[LabeledPoint] = {
+//    data map { case (feature, Relevance(relevance)) =>
+//      LabeledPoint(relevance, toVector(feature))
+//    }
+//  }
+//
   private def toVector(feature: Feature): Vector = {
     Vectors dense feature.coordinates.toArray
   }
 }
+
+object MachineLearning extends MachineLearning
