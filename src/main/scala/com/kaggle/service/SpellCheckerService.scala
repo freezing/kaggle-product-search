3215e3b82df8a0787cf7e2d0927e3e3b7e1ab621
Nikola Stojiljkovic <nikolavla@gmail.com>
2016-03-02 00:19:40 +0000

    Added logs to spell checker service

--- src/main/scala/com/kaggle/service/SpellCheckerService.scala
+++ src/main/scala/com/kaggle/service/SpellCheckerService.scala
@@ -1,16 +1,21 @@
 package com.kaggle.service
 
+import java.util.logging.Logger
+
 /**
   * Created by freezing on 01/03/16.
   */
 class SpellCheckerService {
+  val logger = Logger.getLogger(getClass.getName)
   lazy val dictionary = {
-    (CsvReader.readTextFile("/spell_checker_dictionary.txt") map { line =>
+    val ret = (CsvReader.readTextFile("/spell_checker_dictionary.txt") map { line =>
       val cols = line.split(":")
       val key = cols.head
       val values = cols.last.split(",")
       key -> values.toSet
     }).toMap
+    logger.info("Spell checker dictionary completed.")
+    ret
   }
 
   def getMatches(key: String) = dictionary.getOrElse(key, Set.empty[String])
