daea0af9655e0206a794e55b2918dca8eccc199e
Nikola Stojiljkovic <nikolavla@gmail.com>
2016-03-02 20:46:31 +0000

    Lexer splits in-between words and letters now

--- src/main/scala/com/kaggle/nlp/DataLexer.scala
+++ src/main/scala/com/kaggle/nlp/DataLexer.scala
@@ -4,10 +4,9 @@ package com.kaggle.nlp
   * Created by freezing on 28/02/16.
   */
 class DataLexer extends Serializable {
-  // TODO: Not implemented
   def tokenize(data: String): List[Token] =
     (data.split("[\\s-,/\\(\\)\\.:]+") filter { _.length > 0 }).toList flatMap { x =>
       // Split letters and numbers in between
-      x.split("(?<=\\D)(?=\\d)|(?<=\\d)(?=\\D)") map { t => Token(t) }
+      """\d+|\D+""".r.findAllIn(x) map { t => Token(t) }
     }
 }
